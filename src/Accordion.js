f6235f40d095fd5ac85601579fc6c4512696c6d5
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-15 16:57:59 +0200

    first commit

---
+++ src/Accordion.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Accordion() {
+  return (
+   <div className='accordion'>
+<div className='accordionHeading'>
+    <div className='container'>
+        <p>Question</p>
+        <span>CLICK ME</span>
+    </div>
+     </div>
+     <div className='accordionContent'>
+        <div className='container'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis numquam sed vitae? Iusto, quo distinctio!</p>
+        </div>
+     </div>
+   </div>
+  )
+}
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React ,{useState} from 'react';
+import './App.css';
+import Accordion from './Accordion';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
