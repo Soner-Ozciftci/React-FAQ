f43dc7f16d86ee6097f1ae20c0a5b9d2267ec6da
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-15 17:02:05 +0200

    accordion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React ,{useState} from 'react';
 import './App.css';
 import Accordion from './Accordion';
-
-
-
 function App() {
 
   return (
     <div className="App">
-
+<Accordion/>
     </div>
   );
 }
